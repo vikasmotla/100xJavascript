4fa32087f65ae3a6265673a60983cf1273303f51
vikasmotla <vikas.motla@gmail.com>
2023-12-06 19:04:39 +0530

    anagram easy

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 = str1.toLowerCase().split("").sort().join(""); 
+  str2 = str2.toLowerCase().split("").sort().join("");
+  return str1 === str2;
 }
+/*
+split converts string to array, takes arg to split from, if empty all char will be splitted
+join takes a seperator for each char.
+e.g : [1,2,3].join(",");  ,1,2,3;
+
+More Info can be done using, indexOf and includes with for loop:
+  can be used on arr and string
+  indexOf was created way before includes.
+  [NaN].indexOf(NaN) // => -1 (not found)
+  [NaN].includes(NaN) // => true
+  No difference in performance : https://ryanpeden.com/javascript-indexof-vs-includes-performance/
+*/
+
+// isAnagram('Debit Card', 'Bad Credit');
 
 module.exports = isAnagram;
